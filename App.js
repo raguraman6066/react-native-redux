cb0849acc6f65067622de535d475e2fe489b37f0
raguraman <raguraman.mps6066@gmail.com>
2024-08-02 16:08:36 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { Provider, useDispatch, useSelector } from "react-redux";
+import store from "./store";
+import { increment, decrement, incrementByAmount } from "./slices/counterSlice";
+
+function Counter() {
+  const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.value);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.counterText}>{count}</Text>
+      <Button title="Increment" onPress={() => dispatch(increment())} />
+      <Button title="Decrement" onPress={() => dispatch(decrement())} />
+      <Button
+        title="Increment by 5"
+        onPress={() => dispatch(incrementByAmount(5))}
+      />
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <Counter />
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  counterText: {
+    fontSize: 48,
+    marginBottom: 20,
   },
 });
